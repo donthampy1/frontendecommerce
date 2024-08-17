2fb8910e49e24f015f32ca8cba7f788acad868ec
donthampy1 <150425930+donthampy1@users.noreply.github.com>
2024-08-17 06:25:48 +0530

    Update LaptopForm.jsx

--- src/Pages/Addproductforms/LaptopForm.jsx
+++ src/Pages/Addproductforms/LaptopForm.jsx
@@ -21,7 +21,7 @@ const LaptopForm = ({ category }) => {
     formData.append('category', category); 
 
     try {
-      const response = await axios.post('http://localhost:3000/addproducts/laptop/add', formData, {
+      const response = await axios.post('https://ecommerce-backend-1-s235.onrender.com/addproducts/laptop/add', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
